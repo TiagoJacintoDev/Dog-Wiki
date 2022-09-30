4f7073bbd1762bef52a86f47476b090e73e176c1
TiagoJacintoDev <tiagojacintodev@gmail.com>
2022-09-30 17:07:00 +0100

    Implement Routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home';
-import { Dogs } from './Pages/Dogs';
+import { BreedList } from './Pages/BreedList';
+import { useQuery } from '@tanstack/react-query';
+import { axiosInstance } from './services/axiosInstance';
 
 export default function App() {
+  const {
+    data: dogs,
+    isLoading,
+    error,
+  } = useQuery(['dogsData'], async () => {
+    const res = await axiosInstance.get('breeds');
+    return res.data;
+  });
+
+  if (isLoading) return <p>Loading...</p>;
+
+  if (error) return <p>An error has occurred: {error.message}</p>;
+
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/dogs' element={<Dogs />} />
+      <Route path='/' element={<Home dogs={dogs} />} />
+      <Route path='/dogs' element={<BreedList dogs={dogs} />} />
     </Routes>
   );
 }
