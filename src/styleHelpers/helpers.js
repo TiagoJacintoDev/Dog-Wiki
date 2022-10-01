67899d04a94c4f3d7b4b969acf12c25efc2e4833
TiagoJacintoDev <tiagojacintodev@gmail.com>
2022-10-01 14:42:09 +0100

    Add Style Helpers

--- src/styleHelpers/helpers.js
+++ src/styleHelpers/helpers.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { variables as v } from './variables';
+import { Link as LinkH } from 'react-router-dom';
 
 export const Container = styled.div`
   border-radius: 40px;
@@ -7,4 +8,37 @@ export const Container = styled.div`
   @media (min-width: ${v.ds.laptop}) {
     margin-inline: 10%;
   }
+`;
+
+export const BreedLink = styled(LinkH)`
+  text-decoration: none;
+  color: white;
+  width: 300px;
+  margin-bottom: ${v.size.small};
+  flex-grow: 1;
+`;
+
+export const Grid = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 5%;
+`;
+
+export const DogImg = styled.img`
+  object-fit: cover;
+  aspect-ratio: 4 / 3;
+  border-radius: 20px;
+  margin-bottom: ${v.size.smallest};
+
+  transition: scale 250ms;
+  ${({ isHovering }) => isHovering && `scale: 0.95`}
+`;
+
+export const Caption = styled.h3`
+  ${({ isHovering }) => isHovering && `color: ${v.color.accent}`}
+`;
+
+export const SectionTitle = styled.h2`
+  font-size: ${v.fs.largeLarger};
+  font-weight: ${v.fw.bold};
 `;
